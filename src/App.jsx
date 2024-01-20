b74aadb5491498ac29f04df8dada352b5c781888
oguzhanbayoglu <oguzhanbayoglu1@gmail.com>
2024-01-20 16:29:46 +0300

    file upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ColorPicker from "./ColorPicker";
+
+function App() {
+  return (
+    <div className="App bg-black h-full">
+      <ColorPicker />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ColorPicker.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ColorPicker = () => {
+  return (
+    <div className="text-white h-full w-full flex items-center justify-center">
+      Color Picker
+    </div>
+  );
+};
+
+export default ColorPicker;
